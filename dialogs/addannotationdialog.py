f26bb7d298b3d30935a661046666ccf4d94275eb
Timo Homburg <timo.homburg@gmx.de>
2022-02-04 22:55:27 +0100

    Changes to add dialog

--- dialogs/addannotationdialog.py
+++ dialogs/addannotationdialog.py
@@ -5,10 +5,18 @@ import os.path
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'ui/addannotationdialog.ui'))
 
-
 class AddAnnotationDialog(QDialog, FORM_CLASS):
 
-    def __init__(self, selectedresources, activelayer):
+    def __init__(self, selectionListView,annotationrel="",annotationcontent=""):
         super(AddAnnotationDialog, self).__init__()
         self.setupUi(self)
+        self.selectionListView=selectionListView
+        self.annotationTypeCBox.currentIndexChanged.connect(self.annotationTypeChanged)
         self.show()
+
+    def annotationTypeChanged(self):
+        if self.annotationTypeCBox.currentText=="TextAnnotation":
+            self.textAnnotationEdit.show()
+        elif self.annotationTypeCBox.currentText=="Semantic Web":
+            print("test")
+
--- dialogs/annotationdialog.py
+++ dialogs/annotationdialog.py
@@ -4,6 +4,8 @@ from qgis.PyQt import QtGui
 from qgis.PyQt import uic
 import os.path
 
+from .addannotationdialog import AddAnnotationDialog
+
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'ui/annotationdialog.ui'))
 class AnnotateDialog(QDialog,FORM_CLASS):
@@ -13,8 +15,15 @@ class AnnotateDialog(QDialog,FORM_CLASS):
         self.setupUi(self)
         model = QtGui.QStandardItemModel()
         self.selectionListView.setModel(model)
+        self.addAnnotationButton.clicked.connect(lambda: AddAnnotationDialog(self.selectionListView).exec())
+        self.editAnnotationButton.clicked.connect(self.editAnnotationFunc)
+        self.removeAnnotationButton.clicked.connect(lambda: self.selectionListView.removeItem(self.selectionListView.currentIndex()))
         for i in selectedresources:
             item = QtGui.QStandardItem(str(activelayer.name())+": "+str(i.id()))
             model.appendRow(item)
         self.show()
-         
+
+    def editAnnotationFunc(self):
+        selected=self.selectionListView.selectionModel().selectedIndexes()[0]
+        AddAnnotationDialog(self.selectionListView,selected.data(256),selected.data(257))
+
